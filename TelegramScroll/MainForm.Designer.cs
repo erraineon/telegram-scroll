68d38dddd22bfac74c255aede93797f753b6ff24
Errai <erraineon@gmail.com>
2018-10-06 19:01:38 +0200

    get the juice baby

---
+++ TelegramScroll/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace TelegramScroll
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox
+            // 
+            this.checkBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(114, 75);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(49, 23);
+            this.checkBox.TabIndex = 0;
+            this.checkBox.Text = "enable";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.OnCheckBoxCheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TelegramScroll.Properties.Resources.DdtLrNDVwAAUi91;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(286, 174);
+            this.Controls.Add(this.checkBox);
+            this.Name = "MainForm";
+            this.Text = "stop that shit";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox;
+    }
+}
+
---
+++ TelegramScroll/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Process.NET;
+using Process.NET.Memory;
+using Process.NET.Patterns;
+
+namespace TelegramScroll
+{
+    public partial class MainForm : Form
+    {
+        private IntPtr _address;
+        private ProcessSharp _processSharp;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            try
+            {
+                var telegramProcess = System.Diagnostics.Process.GetProcessesByName("Telegram").FirstOrDefault();
+                if (telegramProcess == null) throw new Exception("telegram isn't running");
+                _processSharp = new ProcessSharp(telegramProcess, MemoryType.Local);
+                _processSharp.Memory = new ExternalProcessMemory(_processSharp.Handle);
+                var scanner = new PatternScanner(_processSharp.ModuleFactory.MainModule);
+                var pattern = new DwordPattern("EB 02 33 DB 83 BF FC 00 00 00 00");
+                var scanResult = scanner.Find(pattern);
+                if (!scanResult.Found) throw new Exception("something broke");
+                _address = scanResult.ReadAddress + pattern.GetBytes().Count;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void OnCheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked)
+            {
+                // xor eax, eax; nop
+                _processSharp.Memory.Write(_address, new byte[] {0x33, 0xC0, 0x90});
+                checkBox.Text = "disable";
+            }
+            else
+            {
+                // mov eax, dword [ebp+8]
+                _processSharp.Memory.Write(_address, new byte[] {0x8b, 0x45, 0x08});
+                checkBox.Text = "enable";
+            }
+        }
+    }
+}
